b88d7792f26a7457fb528d38e1e841c3ea761e49
xiaoosi <931458407@qq.com>
2018-06-25 14:31:45 +0800

    第三次提交

--- my_demo_app/views.py
+++ my_demo_app/views.py
@@ -1,25 +1,28 @@
-#coding: utf-8
+# coding: utf-8
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import os
 import json
 import creat_gif
-from PIL import Image
-#import moviepy.editor as mpy
 
 # Create your views here.
 
 
 def index(request):
-    if not request.session.get('has_session'):
-        request.session['has_session'] = True
-    print(request.session.session_key)
-    test_list = json.loads(request.body)
-    print(test_list)
-    creat_gif.create_text_clip_use_patchs(test_list,
-                                          os.path.join("my_demo_app/static/image_file", request.session.session_key))
-    response2 = JsonResponse({"name": "xiaosi"})
-    return response2
+    if request.method == 'POST':
+        if not request.session.get('has_session'):
+            request.session['has_session'] = True
+        print(request.session.session_key)
+        test_list = json.loads(request.body)
+        print(test_list)
+        try:
+            creat_gif.create_gif_use_patchs(test_list,
+                                            os.path.join("my_demo_app/static/image_file", request.session.session_key))
+            return JsonResponse({"massage": "success"})
+        except Exception, e:
+            return JsonResponse({"massage": "flase"})
+    else:
+        return JsonResponse({"massage": "false"})
 
 
 def creat(request):
@@ -40,14 +43,24 @@ def upload_ajax(request):
         print(sessionid)
         file_obj = request.FILES.get('file')
         if os.path.splitext(file_obj.name)[1] == '.gif':
-            if not sessionid in os.listdir('my_demo_app/static/image_file'):
+
+            if sessionid not in os.listdir('my_demo_app/static/image_file'):
                 os.mkdir(os.path.join('my_demo_app/static/image_file', sessionid))
                 os.mkdir(os.path.join('my_demo_app/static/image_file', sessionid, "patch"))
+
             f = open(os.path.join('my_demo_app/static/image_file', sessionid, 'gif.gif'), 'wb')
             for chunk in file_obj.chunks():
                 f.write(chunk)
             f.close()
-            num = creat_gif.division(os.path.join("my_demo_app/static/image_file", sessionid))
-            return JsonResponse({"file_path": sessionid,
-                                 "img_num": num})
-        return HttpResponse('FALSE')
+
+            try:
+                num = creat_gif.division(os.path.join("my_demo_app/static/image_file", sessionid))
+                return JsonResponse({"file_path": sessionid,
+                                     "img_num": num,
+                                     "massage": "success"})
+            except Exception, e:
+                return JsonResponse({"massage": "false"})
+        else:
+            return JsonResponse({"massage": "false"})
+    else:
+        return JsonResponse({"massage": "false"})
